b77464c179ee4ac840766abad59f82155c7c0c8b
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-01-27 09:31:50 -0700

    Added to repo

---
+++ src/Model/Node.h
@@ -0,0 +1,25 @@
+/*
+ * Node.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: bkon4208
+ */
+
+#ifndef MODEL_NODE_H_
+#define MODEL_NODE_H_
+
+template <class Type>
+
+class Node
+{
+private:
+	Type value;				//Container for the value stored in the node.
+	Node * pointers;		//Pointer to the array of Nodes that are linked to this Node
+public:
+	Node();
+	virtual ~Node();
+	Type getValue();
+	Node * getPointers();
+};
+
+#endif /* MODEL_NODE_H_ */
