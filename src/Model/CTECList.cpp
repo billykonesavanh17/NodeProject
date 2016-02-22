770101729394e00b6d7e205ae3c147f2e9b948cc
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-02-22 09:22:19 -0700

    Added CTECList

---
+++ src/Model/CTECList.cpp
@@ -0,0 +1,20 @@
+/*
+ * CTECList.cpp
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: bkon4208
+ */
+
+#include "CTECList.h"
+
+CTECList::CTECList()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CTECList::~CTECList()
+{
+	// TODO Auto-generated destructor stub
+}
+
